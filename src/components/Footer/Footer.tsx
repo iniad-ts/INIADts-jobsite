b7e37166648f26ae45e7bc35d579e6f985d2c6d6
imoken777 <imoken89@gmail.com>
2023-08-06 18:38:46 +0900

    add: Footerコンポーネントを仮で作成

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.body} />
+    </div>
+  );
+};
