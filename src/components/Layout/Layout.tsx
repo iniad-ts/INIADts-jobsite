cd6286a8bb431eae7a6b1e99cff2d3fbd31456ee
mst-mkt <s1f102303057@iniad.org>
2023-08-06 20:10:23 +0900

    add: 仮のレイアウトにコンポーネントを当てはめてみた

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,15 @@
+import { Footer } from '../Footer/Footer';
+import { Header } from '../Header/Header';
+import { SideBar } from '../SideBar/SideBar';
 import styles from './layout.module.css';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={styles.container}>
-      <header className={styles.header} />
-      <aside className={styles.sidebar} />
+      <Header />
+      <SideBar array={[]} />
       <main className={styles.main}>{children}</main>
-      <footer className={styles.footer} />
+      <Footer />
     </div>
   );
 };
