5ea5311cffa281f88f6527d7ebcc01207e4553c9
imoken777 <imoken89@gmail.com>
2023-08-07 17:18:06 +0900

    fotterデザイン

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,29 @@
 import styles from './Footer.module.css';
 
 export const Footer = () => {
+  const Circle = () => {
+    return <svg className={styles.circle} />;
+  };
   return (
     <div className={styles.container}>
+      <div className={styles.tab}>
+        <div className={styles.tabItem}>PROBLEMS</div>
+        <div className={styles.tabItem}>OUTPUT</div>
+        <div className={styles.tabItem}>DEBUG CONSOLE</div>
+        <div className={`${styles.tabItem} ${styles.choice}`}>TERMINAL</div>
+        <div className={styles.tabItem}>PORTS</div>
+      </div>
       <div className={styles.body}>
-        <p className={styles.p}>PS C:\Toyo\INIAD\INIAD.ts{'>'} git contact -v</p>
+        <p className={styles.p}>
+          <Circle />
+          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>git</span> contact -v
+        </p>
         <p className={styles.p}>INIAD.ts メールアドレスなどを張る</p>
-        <p className={styles.p}>PS C:\Toyo\INIAD\INIAD.ts{'>'} git </p>
-        <p className={styles.p}>created at 2023</p>
+        <p className={styles.p}>
+          <Circle />
+          PS C:\Toyo\INIAD\INIAD.ts{'>'} <span className={styles.git}>git</span> copyright -v
+        </p>
+        <p className={styles.p}>© 2023 INIAD.ts</p>
       </div>
     </div>
   );
