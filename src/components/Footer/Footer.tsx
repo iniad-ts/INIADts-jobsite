c86dff881732e326bc34e1441aa156a2cca5a6ad
imoken777 <imoken89@gmail.com>
2023-08-07 16:11:40 +0900

    Footerの色と文をちょっとかいてみた

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import styles from './Footer.module.css';
 export const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.body} />
+      <div className={styles.body}>
+        <p>PS C:\Toyo\INIAD\INIAD.ts{'>'} git contact -v</p>
+        <p>INIAD.ts メールアドレスなどを張る</p>
+        <p>PS C:\Toyo\INIAD\INIAD.ts{'>'} git </p>
+        <p>created at 2023</p>
+      </div>
     </div>
   );
 };
