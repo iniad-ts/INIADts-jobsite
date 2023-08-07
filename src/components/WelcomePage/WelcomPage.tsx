8d84cb1df47347bef02934e08e262d98947d45c9
s1f10230014 <s1f102300144@iniad.org>
2023-08-07 16:00:44 +0900

    #追加

--- src/components/WelcomePage/WelcomPage.tsx
+++ src/components/WelcomePage/WelcomPage.tsx
@@ -8,11 +8,11 @@ export const WelcomePage = () => {
           #INIAD.ts
         </div>
         <div className={styles.normal_row}>INIAD.ts就職サイトへようこそ</div>
-        <div className={styles.slash_row}>就活サイトである旨</div>
+        <div className={styles.slash_row}>#就活サイトである旨</div>
         <div className={styles.normal_row}>oooooooooooooooooooです</div>
-        <div className={styles.slash_row}>TSがメインである旨</div>
+        <div className={styles.slash_row}>#TSがメインである旨</div>
         <div className={styles.normal_row}>oooooooooooooooooooです</div>
-        <div className={styles.slash_row}>最終更新</div>
+        <div className={styles.slash_row}>#最終更新</div>
         <div className={styles.normal_row}>2023/08/01</div>
       </div>
     </div>
