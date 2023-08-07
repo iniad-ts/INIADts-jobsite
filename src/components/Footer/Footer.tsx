e184b3d3858d86bf089a3764695e40857c0bb2bc
imoken777 <imoken89@gmail.com>
2023-08-07 16:21:53 +0900

    Resolved conflicts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,12 @@ import styles from './Footer.module.css';
 export const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.body} />
+      <div className={styles.body}>
+        <p className={styles.p}>PS C:\Toyo\INIAD\INIAD.ts{'>'} git contact -v</p>
+        <p className={styles.p}>INIAD.ts メールアドレスなどを張る</p>
+        <p className={styles.p}>PS C:\Toyo\INIAD\INIAD.ts{'>'} git </p>
+        <p className={styles.p}>created at 2023</p>
+      </div>
     </div>
   );
 };
-////
