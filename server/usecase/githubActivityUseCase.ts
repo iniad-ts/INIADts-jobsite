5492a2fd090a06402ed6522d5f74f560c4e8c2b8
mst-mkt <s1f102303057@iniad.org>
2023-08-10 07:52:58 +0900

    add: 特定のユーザーのデータをs3から取得する処理を実装

--- server/usecase/githubActivityUseCase.ts
+++ server/usecase/githubActivityUseCase.ts
@@ -1,6 +1,6 @@
 import { GITHUB_API_ORIGIN, GITHUB_USERNAME, S3_BUCKET } from '$/service/envValues';
 import { s3Client } from '$/service/s3Client';
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { GetObjectCommand, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const uploadActivity = async (userName: string, data: string) => {
   const params = {
@@ -17,13 +17,13 @@ const uploadActivity = async (userName: string, data: string) => {
   }
 };
 
-const fetchActivity = async (userName: string) => {
-  const res = await fetch(`${GITHUB_API_ORIGIN}v4/${userName}`);
+const fetchActivity = async (userId: string) => {
+  const res = await fetch(`${GITHUB_API_ORIGIN}v4/${userId}`);
   const data = await res.json();
 
   if (data !== null) {
     const jsonString = JSON.stringify(data);
-    uploadActivity(userName, jsonString);
+    uploadActivity(userId, jsonString);
   }
 };
 
@@ -31,6 +31,35 @@ const fetchAllActivity = () => {
   GITHUB_USERNAME.map((userName) => fetchActivity(userName));
 };
 
+const getLatestActivity = async (userId: string) => {
+  const params = {
+    Bucket: S3_BUCKET,
+    Prefix: `github-activity/${userId}/`,
+  };
+
+  try {
+    const command = new ListObjectsCommand(params);
+    const { Contents } = await s3Client.send(command);
+
+    if (Contents !== undefined) {
+      const latestObjectKey = Contents[0].Key;
+      const latestObjectParams = {
+        Bucket: S3_BUCKET,
+        Key: latestObjectKey,
+      };
+
+      const latestObjectCommand = new GetObjectCommand(latestObjectParams);
+      const { Body } = await s3Client.send(latestObjectCommand);
+
+      const data = JSON.parse((Body ?? '').toString());
+      return data;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export const githubActivityUseCase = {
   fetchAllActivity,
+  getLatestActivity,
 };
