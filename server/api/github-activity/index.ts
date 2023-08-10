63ca59a9200adb894eaaa1e4925d57dcfec00e99
mst-mkt <s1f102303057@iniad.org>
2023-08-10 16:00:50 +0900

    fix: 型エラーを修正

--- server/api/github-activity/index.ts
+++ server/api/github-activity/index.ts
@@ -1,5 +1,5 @@
-import type { GitHubActivityModel } from '$/commonTypesWithClient/models';
 import type { DefineMethods } from 'aspida';
+import type { GitHubActivityModel } from 'commonTypesWithClient/models';
 
 export type Methods = DefineMethods<{
   get: {
