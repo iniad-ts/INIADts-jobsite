d964df27553edefc8dd7bc4b4df58a69941929c6
mst-mkt <s1f102303057@iniad.org>
2023-08-12 15:46:34 +0900

    add: メンバーの情報を操作するRepositoryを作成

---
+++ server/repository/membersRepository.ts
@@ -0,0 +1,69 @@
+import type { MemberModel } from '$/commonTypesWithClient/models';
+import { S3_BUCKET } from '$/service/envValues';
+import { s3Client } from '$/service/s3Client';
+import { GetObjectCommand, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { assert } from 'vitest';
+
+export const membersRepository = {
+  upsert: async (memberInfo: MemberModel) => {
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: `members/${memberInfo.githubId}.json`,
+      Body: JSON.stringify(memberInfo),
+    };
+
+    try {
+      const s3Command = new PutObjectCommand(s3Params);
+      await s3Client.send(s3Command);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  getMemberList: async () => {
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Prefix: 'members/',
+    };
+
+    try {
+      const s3Command = new ListObjectsCommand(s3Params);
+      const { Contents } = await s3Client.send(s3Command);
+
+      if (Contents === undefined) return [];
+
+      const memberList = await Promise.all(
+        Contents.map(({ Key }) => {
+          assert(Key !== undefined);
+          const githubId = Key.split('/')[1].split('.')[0];
+          return githubId;
+        })
+      );
+
+      return memberList;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  },
+  getMember: async (githubId: string): Promise<MemberModel | null> => {
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: `members/${githubId}.json`,
+    };
+
+    try {
+      const s3Command = new GetObjectCommand(s3Params);
+      const s3Response = await s3Client.send(s3Command);
+
+      if (s3Response.Body === undefined) return null;
+
+      const memberInfoString = await s3Response.Body.transformToString();
+      const memberInfo: MemberModel = JSON.parse(memberInfoString);
+
+      return memberInfo;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+};
