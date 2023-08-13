ab3d6024a57d1c0f91794a1054d78265d1cbf219
mst-mkt <s1f102303057@iniad.org>
2023-08-13 20:37:38 +0900

    fix: Repoの変更のためUseCaseの処理を削除

--- server/usecase/memberUseCase.ts
+++ server/usecase/memberUseCase.ts
@@ -1,17 +1,3 @@
-import type { MemberModel } from '$/commonTypesWithClient/models';
-import { membersRepository } from '$/repository/membersRepository';
-import assert from 'assert';
-
 export const memberUseCase = {
-  getAllMembers: async (): Promise<MemberModel[]> => {
-    const memberList = await membersRepository.getMemberList();
-    const allMembers = await Promise.all(
-      memberList.map(async (githubId) => {
-        const member = await membersRepository.getMember(githubId);
-        assert(member !== null, 'member is null');
-        return member;
-      })
-    );
-    return allMembers;
-  },
+  //
 };
