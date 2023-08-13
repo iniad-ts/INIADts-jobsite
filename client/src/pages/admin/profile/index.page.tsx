f6ca1df83cb02ce551c1821fdc6bbe86b1f00c31
s1f10230014 <s1f102300144@iniad.org>
2023-08-13 19:43:43 +0900

    プロフィール編集画面に検索した情報を表示

--- client/src/pages/admin/profile/index.page.tsx
+++ client/src/pages/admin/profile/index.page.tsx
@@ -1,14 +1,54 @@
 import type { MemberModel } from 'commonTypesWithClient/models';
+import Head from 'next/head';
 import { useState } from 'react';
 import styles from './index.module.css';
 
 const AdminProfile = () => {
   const [member, setmember] = useState<MemberModel>();
+
+  const serchMenber = () => {
+    //特定のメンバーを取得するapiをたたく
+    console.log('取得');
+  };
   return (
     <div className={styles.container}>
+      <Head>
+        <title>EditMemberInfo | INIAD.ts</title>
+      </Head>
       <div className={styles.serchTable}>
-        <button>検索</button>
+        <button onClick={() => serchMenber()}>検索</button>
         <input id="idForm" type="text" name="idForm" placeholder="githubIdを入力してください" />
+      </div>
+      <h2>Profile</h2>
+      <div className={styles.plafileTable}>
+        <table>
+          <tbody>
+            <tr>
+              <th>githubId:</th>
+              <td>{member?.githubId}</td>
+            </tr>
+            <tr>
+              <th>displayName:</th>
+              <td>{member?.displayName}</td>
+            </tr>
+            <tr>
+              <th>avatarURL:</th>
+              <td>{member?.avatarURL}</td>
+            </tr>
+            <tr>
+              <th>graduateYear:</th>
+              <td>{member?.graduateYear}</td>
+            </tr>
+            <tr>
+              <th>introduction:</th>
+              <td>{member?.introduction}</td>
+            </tr>
+            <tr>
+              <th>links:</th>
+              <td>{member?.links}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
