22ad2314797df4ccdc00ee127d703aae4def8bec
mst-mkt <s1f102303057@iniad.org>
2023-08-13 20:20:35 +0900

    fix: schemeとtsの型の表記ゆれを修正

--- server/repository/membersRepository.ts
+++ server/repository/membersRepository.ts
@@ -1,13 +1,11 @@
 import type { MemberModel } from '$/commonTypesWithClient/models';
-import { S3_BUCKET } from '$/service/envValues';
-import { s3Client } from '$/service/s3Client';
-import { GetObjectCommand, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { prismaClient } from '$/service/prismaClient';
 import type { Member } from '@prisma/client';
 import { z } from 'zod';
 
 const toMemberModel = (prismaMember: Member): MemberModel => ({
   githubId: prismaMember.githubId,
-  displayName: prismaMember.userName,
+  displayName: prismaMember.displayName,
   realName: prismaMember.realName,
   graduateYear: z.number().min(2000).max(3000).parse(prismaMember.graduateYear),
   introduction: prismaMember.introduction,
