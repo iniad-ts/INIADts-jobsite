6bef852e7dbe488811c0983ab9bd030de30769b7
s1f10230014 <s1f102300144@iniad.org>
2023-08-13 20:17:07 +0900

    Configファイルの消去

--- client/src/components/Config/Config.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Config.module.css';
-
-export const Config = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.body} />
-    </div>
-  );
-};
