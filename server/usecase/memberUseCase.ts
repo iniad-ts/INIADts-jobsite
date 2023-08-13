4ff6956323eec62b3dc0012903f5e5ea26e30360
mst-mkt <s1f102303057@iniad.org>
2023-08-14 07:32:36 +0900

    refactor: memberUseCaseの処理を改善

--- server/usecase/memberUseCase.ts
+++ server/usecase/memberUseCase.ts
@@ -1,56 +1,56 @@
 import type { MemberModel } from '$/commonTypesWithClient/models';
+import type { MemberList } from '$/repository/membersRepository';
 import { membersRepository } from '$/repository/membersRepository';
 
 const createMember = async (member: MemberModel): Promise<MemberModel | null> => {
-  const newMember = await membersRepository.saveToDB(member);
-  await membersRepository.saveToS3(member);
-  if (newMember === null) return null;
+  const [createdMember] = await Promise.all([
+    await membersRepository.saveToDB(member),
+    await membersRepository.saveToS3(member),
+  ]);
 
-  const membersList = await membersRepository.getListFromS3();
-  if (membersList === null) return null;
-  // FIXME: リストがなかったら新規作成すべきかも
+  const memberList: MemberList = (await membersRepository.getListFromS3()) ?? { members: [] };
+  memberList.members.push({ githubId: member.githubId, graduateYear: member.graduateYear });
 
-  const newList = [...membersList.members, newMember];
+  await membersRepository.saveListToS3(memberList);
 
-  await membersRepository.saveListToS3({ members: newList });
-
-  return newMember;
+  return createdMember;
 };
 
 const updateMember = async (member: MemberModel): Promise<MemberModel | null> => {
-  const updatedMember = await membersRepository.saveToDB(member);
-  await membersRepository.saveToS3(member);
+  const [updatedMember] = await Promise.all([
+    await membersRepository.saveToDB(member),
+    await membersRepository.saveToS3(member),
+  ]);
+  if (updatedMember === null) return null;
 
-  const membersList = await membersRepository.getListFromS3();
-  if (membersList === null) return null;
+  const memberList: MemberList | null = await membersRepository.getListFromS3();
+  if (memberList === null) return null;
 
-  const newList = membersList?.members.map((member) => {
-    if (member.githubId === updatedMember?.githubId) {
-      return {
-        ...member,
-        githubId: updatedMember.githubId,
-        graduateYear: updatedMember.graduateYear,
-      };
-    }
-    return member;
-  });
-
-  await membersRepository.saveListToS3({ members: newList });
+  const newMemberList: MemberList = {
+    members: memberList.members.map((member) => {
+      if (member.githubId === updatedMember.githubId) {
+        return { githubId: member.githubId, graduateYear: updatedMember.graduateYear };
+      }
+      return member;
+    }),
+  };
+  membersRepository.saveListToS3(newMemberList);
 
   return updatedMember;
 };
 
 export const memberUseCase = {
   save: async (member: MemberModel): Promise<MemberModel | null> => {
-    const beforeMember = await membersRepository.getFromS3(member.githubId);
+    const currentMember = await membersRepository.getFromS3(member.githubId);
 
-    if (beforeMember === null) {
-      return await createMember(member);
+    if (currentMember === null) {
+      const newMember: MemberModel | null = await createMember(member);
+      return newMember;
     }
 
-    if (beforeMember !== member) {
-      return await updateMember(member);
-    }
-    return null;
+    if (member === currentMember) return currentMember;
+
+    const updatedMember: MemberModel | null = await updateMember(member);
+    return updatedMember;
   },
 };
