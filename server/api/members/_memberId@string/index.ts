6c14836c3c2d46482112c2b6f5c0a723f76e98e3
mst-mkt <s1f102303057@iniad.org>
2023-08-13 21:49:55 +0900

    fix: typecheckに通るようにパスを修正

--- server/api/members/_memberId@string/index.ts
+++ server/api/members/_memberId@string/index.ts
@@ -1,5 +1,5 @@
-import type { MemberModel } from '$/commonTypesWithClient/models';
 import type { DefineMethods } from 'aspida';
+import type { MemberModel } from 'commonTypesWithClient/models';
 
 export type Methods = DefineMethods<{
   get: {
