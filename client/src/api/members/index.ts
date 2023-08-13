2b9a6e590d644cc7f069c568580f69082d7371e6
mst-mkt <s1f102303057@iniad.org>
2023-08-13 17:20:09 +0900

    add: s3からjsonを取得するaspidaの型定義を設定

---
+++ client/src/api/members/index.ts
@@ -0,0 +1,7 @@
+import type { DefineMethods } from 'aspida';
+import type { MemberModel } from 'commonTypesWithClient/models';
+export type Methods = DefineMethods<{
+  get: {
+    resBody: MemberModel[];
+  };
+}>;
