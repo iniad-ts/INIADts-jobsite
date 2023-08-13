6bbf68d8f81cf23e14d223d4cbb864a2358bd09f
1f10230234 <s1f102302348@iniad.org>
2023-08-14 00:00:53 +0900

    add: textbox sizing

--- client/src/components/Contact/Contact.tsx
+++ client/src/components/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import MemberInfo from '../MemberInfo/MemberInfo';
 import styles from './Contact.module.css';
 
@@ -9,6 +10,13 @@ type FormModel = {
 };
 
 const InputForm = ({ id, name, placeholder, label }: FormModel) => {
+  const [length, setWidth] = useState(placeholder.length * 15);
+  const elm = useRef(null);
+  const handleTextareaChange = (event: any) => {
+    elm.current = event.target.value;
+    console.log(elm.current);
+    setWidth(Math.max(placeholder.length * 15, [...(elm.current ?? [])].length * 15));
+  };
   return (
     <div className={styles.column}>
       <label htmlFor={id} className={styles.key}>
@@ -21,33 +29,53 @@ const InputForm = ({ id, name, placeholder, label }: FormModel) => {
         type="text"
         name={name}
         placeholder={placeholder}
-        onChange={() => null}
+        ref={elm}
+        onChange={handleTextareaChange}
+        style={{ width: length }}
       />
       <label htmlFor={id}>,</label>
     </div>
   );
 };
 
-const TextForm = ({ id, name, placeholder, label }: FormModel) => (
-  <div className={styles.column2}>
-    <label htmlFor="company" className={styles.key}>
-      {'  '}
-      {label}:{' '}
-    </label>
-    <div>
-      <textarea className={styles.input} id={id} name={name} placeholder={placeholder} />
+const TextForm = ({ id, name, placeholder, label }: FormModel) => {
+  const [length, setWidth] = useState(120);
+  const elm = useRef(null);
+  const handleTextareaChange = (event: any) => {
+    // テキストエリアの内容が変更されるたびに useRef を更新
+    // const element = elm.current.getBoundingClientRect();
+    // console.log(JSON.stringify(element));
+    elm.current = event.target.value;
+    console.log(elm.current);
+    setWidth(Math.max(120, [...(elm.current ?? [''])].length * 15));
+  };
+  return (
+    <div className={(styles.column2, styles.wrap)}>
+      <label htmlFor="company" className={styles.key}>
+        {'  '}
+        {label}:{' '}
+      </label>
+      <textarea
+        className={styles.textarea}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        ref={elm}
+        onChange={handleTextareaChange}
+        style={{ width: length }}
+      />
     </div>
-  </div>
-);
+  );
+};
 
 const SubmitButton = () => (
   <button type="submit" name="button" value="送信">
     送信
   </button>
 );
+const office = { 概要: 'aaa', お申込み: [] };
 
 export const Contact = () => {
-  const office = { 概要: 'aaa', お申込み: [] };
   return (
     <div className={styles.container}>
       <MemberInfo name="オフィスの見学" value={office} />
@@ -68,7 +96,7 @@ export const Contact = () => {
           placeholder="あかばね（株）"
           label="会社名"
         />
-        <InputForm id="name" name="entry.2124166764" placeholder="件名" label="件名" />
+        <InputForm id="title" name="entry.2124166764" placeholder="件名" label="件名" />
         <TextForm id="company" name="entry.286334661" placeholder="お問い合わせ内容" label="内容" />
         <span className={styles['bracket-3']}>{'}'}</span>
         {';'}
