223cf6d90e7bd94fc27a34e620c0b449833039de
mst-mkt <s1f102303057@iniad.org>
2023-08-13 18:02:58 +0900

    fix: apiの構造を変更

--- client/src/api/members/_memberId@string/info.json/index.ts
+++ client/src/api/members/_memberId@string/info.json/index.ts
@@ -2,6 +2,6 @@ import type { DefineMethods } from 'aspida';
 import type { MemberModel } from 'commonTypesWithClient/models';
 export type Methods = DefineMethods<{
   get: {
-    reqBody: MemberModel;
+    resBody: MemberModel;
   };
 }>;
