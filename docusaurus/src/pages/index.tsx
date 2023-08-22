68a57f3582dcecabf8e3b0d7c667cfd40b35cb74
mst-mkt <s1f102303057@iniad.org>
2023-08-22 23:11:34 +0900

    add: メインページをiniad.ts用に変更

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -1,40 +1,45 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Layout from '@theme/Layout';
-import clsx from 'clsx';
 import React from 'react';
 import { AuthLoader } from '../components/AuthLoader/AuthLoader';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`${siteConfig.title}`} description="job site for INIAD.ts">
       <AuthLoader />
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div className={styles.top}>
+          <h1 className={styles.title}>INIAD.ts</h1>
+          <p>INIAD公認サークル(予定)・INIAD.tsのサイトです</p>
+        </div>
+        <div className={styles.section}>
+          <h2># About INIAD.ts</h2>
+          <p className={styles.text}>
+            ここにINIAD.tsについての説明を書く
+            <br />
+            typescriptを主に使用している旨、 <br />
+            また、どのような活動をしているかを書く
+          </p>
+          <div className={styles.link}>
+            <Link to="/about">INIAD.tsメンバー一覧</Link>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <h2># About this site</h2>
+          <p className={styles.text}>
+            この就職サイトについての説明を書く
+            <br />
+            我々の目的や、双方へのメリットを記載する
+            <br />
+            また、どのような機能やページがあるかをわかりやすくまとめる
+          </p>
+          <div className={styles.link}>
+            <Link to="/contact">お問い合わせ</Link>
+          </div>
+        </div>
       </main>
     </Layout>
   );
