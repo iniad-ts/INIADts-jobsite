611fd9633e43e2670f4c809955e8710f33872671
1F10220146 <s1f102201469@iniad.org>
2023-08-23 00:11:33 +0900

    メンバー用のページを作成

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -82,15 +82,15 @@ const config = {
             position: 'left',
             items: [
               {
-                to: '/members#25',
+                to: '/docs/member/2025',
                 label: '25卒',
               },
               {
-                to: '/members#26',
+                to: '/docs/member/2026',
                 label: '26卒',
               },
               {
-                to: '/members#27',
+                to: '/docs/member/2027',
                 label: '27卒',
               },
             ],
