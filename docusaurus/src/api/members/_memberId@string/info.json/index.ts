c2fcb5365099435e729382dd706599d23b98c90f
mst-mkt <s1f102303057@iniad.org>
2023-08-22 19:22:48 +0900

    refactor: 行間を開けて調節

--- docusaurus/src/api/members/_memberId@string/info.json/index.ts
+++ docusaurus/src/api/members/_memberId@string/info.json/index.ts
@@ -1,5 +1,6 @@
 import type { DefineMethods } from 'aspida';
 import type { MemberModel } from 'commonTypesWithClient/models';
+
 export type Methods = DefineMethods<{
   get: {
     resBody: MemberModel;
