0f385404e7ef10cac5ce89ec0221a52eb5e9515d
mst-mkt <s1f102303057@iniad.org>
2023-08-24 00:14:43 +0900

    add: adminページのapi取得部分を追加

--- docusaurus/src/pages/admin/profile/index.tsx
+++ docusaurus/src/pages/admin/profile/index.tsx
@@ -1,9 +1,27 @@
+import { userAtom } from '@site/src/atoms/user';
 import { AuthLoader } from '@site/src/components/AuthLoader/AuthLoader';
+import { apiClient } from '@site/src/utils/apiClient';
 import Layout from '@theme/Layout';
-import React from 'react';
+import type { MemberModel } from 'commonTypesWithClient/models';
+import { useAtom } from 'jotai';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './index.module.css';
 
 const Profile = () => {
+  const [user] = useAtom(userAtom);
+  const [member, setMember] = useState<MemberModel | null>(null);
+
+  const fetchMember = useCallback(async () => {
+    if (user === null) return;
+    const res = await apiClient.members._memberId(user.githubId).$get();
+    setMember(res);
+  }, [user]);
+
+  useEffect(() => {
+    const fetchIntervalId = setInterval(fetchMember, 1000);
+    return () => clearInterval(fetchIntervalId);
+  }, [fetchMember]);
+
   return (
     <Layout title="Profile">
       <AuthLoader />
