d76ca70dffd619becb8fe71f2092d758d5fc402e
mst-mkt <s1f102303057@iniad.org>
2023-08-23 23:47:02 +0900

    add: admin用のページ(/admin)を追加

--- docusaurus/src/pages/admin/index.tsx
+++ docusaurus/src/pages/admin/index.tsx
@@ -1,7 +1,26 @@
+import Link from '@docusaurus/Link';
+import { AuthLoader } from '@site/src/components/AuthLoader/AuthLoader';
+import Layout from '@theme/Layout';
 import React from 'react';
+import styles from './index.module.css';
 
 const Admin = () => {
-  return <div />;
+  return (
+    <Layout title="Admin">
+      <div className={styles.container}>
+        <AuthLoader />
+        <h1 className={styles.title}>INIAD.ts 管理者向けページ</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/admin/members">メンバー追加・削除</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/admin/profile">プロフィール編集</Link>
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  );
 };
 
 export default Admin;
