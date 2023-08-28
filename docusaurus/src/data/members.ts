20552dcee4bc7cb8a2342dfdb8c9682720d47f13
s1f10230014 <s1f102300144@iniad.org>
2023-08-29 03:12:11 +0900

    add:mmembersのデータ追加

--- docusaurus/src/data/members.ts
+++ docusaurus/src/data/members.ts
@@ -56,4 +56,58 @@ export const members: Member[] = [
     graduateYear: 2027,
     socialLinks: ['https://github.com/yossuli'],
   },
+  {
+    userName: 'g-ratie',
+    displayName: 'ラティ',
+    graduateYear: 2026,
+    socialLinks: ['https://github.com/g-ratie'],
+  },
+  {
+    userName: 'mst-mkt',
+    displayName: '🧶',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/mst-mkt'],
+  },
+  {
+    userName: 'yossuli',
+    displayName: 'yossuli',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/yossuli'],
+  },
+  {
+    userName: 'g-ratie',
+    displayName: 'ラティ',
+    graduateYear: 2026,
+    socialLinks: ['https://github.com/g-ratie'],
+  },
+  {
+    userName: 'mst-mkt',
+    displayName: '🧶',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/mst-mkt'],
+  },
+  {
+    userName: 'yossuli',
+    displayName: 'yossuli',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/yossuli'],
+  },
+  {
+    userName: 'g-ratie',
+    displayName: 'ラティ',
+    graduateYear: 2026,
+    socialLinks: ['https://github.com/g-ratie'],
+  },
+  {
+    userName: 'mst-mkt',
+    displayName: '🧶',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/mst-mkt'],
+  },
+  {
+    userName: 'yossuli',
+    displayName: 'yossuli',
+    graduateYear: 2027,
+    socialLinks: ['https://github.com/yossuli'],
+  },
 ];
