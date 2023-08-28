39670cc342fc5fe7905772f2ac11db0ae9e9aa3c
s1f10230014 <s1f102300144@iniad.org>
2023-08-29 02:36:54 +0900

    add: react-router-dom

--- docusaurus/src/components/Members/Members.tsx
+++ docusaurus/src/components/Members/Members.tsx
@@ -1,38 +1,16 @@
-import React, { useState } from 'react';
+import { members } from '@site/src/data/members';
+import React from 'react';
 import styles from './Members.module.css';
 
 export const Members = () => {
-  const [members, setmembers] = useState([
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-    'jun-eg',
-    'mst-mkt',
-    'yossuli',
-  ]);
   return (
     <div className={styles.container}>
       {members.map((member, i) => (
         <div key={i} className={styles.member}>
-          <img src={`https://avatars.githubusercontent.com/${member}`} alt={`${member}'s image`} />
+          <img
+            src={`https://avatars.githubusercontent.com/${member.userName}`}
+            alt={`${member}'s image`}
+          />
         </div>
       ))}
     </div>
