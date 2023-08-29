9b58a64efc01320263730ac46c13a9188c0a78d6
mst-mkt <s1f102303057@iniad.org>
2023-08-29 15:05:10 +0900

    remove: serverのAPIと認証周りを削除

--- server/api/health/index.ts
+++
@@ -1,7 +0,0 @@
-import type { DefineMethods } from 'aspida';
-
-export type Methods = DefineMethods<{
-  get: {
-    resBody: Record<'server' | 'db', 'ok' | 'ng'>;
-  };
-}>;
--- server/api/me/index.ts
+++
@@ -1,8 +0,0 @@
-import type { DefineMethods } from 'aspida';
-import type { UserModel } from 'commonTypesWithClient/models';
-
-export type Methods = DefineMethods<{
-  get: {
-    resBody: UserModel;
-  };
-}>;
