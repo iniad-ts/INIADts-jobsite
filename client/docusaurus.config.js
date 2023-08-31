be99ffb8db0a7fec6cc26700cb29f548ddd19925
mst-mkt <s1f102303057@iniad.org>
2023-08-31 21:13:00 +0900

    fix: docusaurusのコンフィグを修正

--- client/docusaurus.config.js
+++ client/docusaurus.config.js
@@ -61,6 +61,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        pages: {
+          exclude: ['**/*.css.d.ts'],
+        },
       }),
     ],
   ],
