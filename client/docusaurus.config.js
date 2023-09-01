c468a6ff883ec158adbbfdd55a43b1b74c142e8d
1F10220146 <s1f102201469@iniad.org>
2023-09-02 00:42:49 +0900

    editUrlを修正

--- client/docusaurus.config.js
+++ client/docusaurus.config.js
@@ -48,15 +48,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/INIAD-Developers/INIADts-jobsite/tree/develop/client/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/INIAD-Developers/INIADts-jobsite/tree/develop/client/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
