4b75aa4574751ee34f1197a050bd90fc388f5d12
mst-mkt <s1f102303057@iniad.org>
2023-09-02 02:16:59 +0900

    fix: 学年を現在の年から計算する

--- client/src/pages/members/index.tsx
+++ client/src/pages/members/index.tsx
@@ -1,6 +1,6 @@
 import { members } from '@site/src/data/members';
 import Layout from '@theme/Layout';
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import styles from './index.module.css';
 
 const Members = () => {
@@ -20,6 +20,13 @@ const Members = () => {
     return memberList;
   }, []);
 
+  const computeGrade = useCallback((graduateYear: number) => {
+    const isBetweenJanAndMar = new Date().getMonth() + 1 <= 3;
+    const currentYear = new Date().getFullYear();
+    const currentGrade = 5 + currentYear - graduateYear - (isBetweenJanAndMar ? 1 : 0);
+    return currentGrade;
+  }, []);
+
   return (
     <Layout title="Members">
       <div className={styles.container}>
@@ -32,7 +39,7 @@ const Members = () => {
             {memberList.map((grade) => (
               <li key={grade.grade} className={styles.grade}>
                 <h2 id={`${grade.grade}`}>
-                  <a href={`#${grade.grade}`}>{2028 - grade.grade}年生</a>
+                  <a href={`#${grade.grade}`}>{computeGrade(grade.grade)}年生</a>
                 </h2>
                 <ul className={styles.memberList}>
                   {grade.members.map((member) => (
