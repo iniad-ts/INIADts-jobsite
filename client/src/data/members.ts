a408ebbd00d78f99fa81e680124221906ab1f7d2
mst-mkt <s1f102303057@iniad.org>
2023-09-03 16:57:36 +0900

    add: 仮のメンバーのデータを充実させた

--- client/src/data/members.ts
+++ client/src/data/members.ts
@@ -6,108 +6,151 @@ export const members: Member[] = [
     userName: 'g-ratie1',
     displayName: 'ラティ',
     graduateYear: 2026,
+    introduction: 'INIAD.ts代表のラティです。よろしくお願いします。',
     socialLinks: ['https://github.com/g-ratie'],
+    products: [
+      {
+        title: 'INIAD.ts入退出Bot',
+        description: 'INIAD.tsの入退出を管理するDiscord Botです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 99,
   },
   {
     userName: 'mst-mkt1',
     displayName: '🧶',
     graduateYear: 2027,
+    introduction: 'INIAD.tsの🧶です。よろしくお願いします。',
     socialLinks: ['https://github.com/mst-mkt'],
+    products: [
+      {
+        title: 'オセロ',
+        description: 'オセロです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 63,
   },
   {
     userName: 'yossuli1',
     displayName: 'yossuli',
     graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
+    introduction: 'INIAD.tsのyossuliです。よろしくお願いします。',
+    socialLinks: ['https://github.com/yossuli', 'https://twitter.com/yossuli'],
+    products: [
+      {
+        title: 'RPG',
+        description: 'RPGです。',
+      },
+    ],
+    findy: 59,
   },
   {
     userName: 'g-ratie2',
     displayName: 'ラティ',
     graduateYear: 2026,
+    introduction: 'INIAD.ts代表のラティです。よろしくお願いします。',
     socialLinks: ['https://github.com/g-ratie'],
+    products: [
+      {
+        title: 'INIAD.ts入退出Bot',
+        description: 'INIAD.tsの入退出を管理するDiscord Botです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 99,
   },
   {
     userName: 'mst-mkt2',
     displayName: '🧶',
     graduateYear: 2027,
+    introduction: 'INIAD.tsの🧶です。よろしくお願いします。',
     socialLinks: ['https://github.com/mst-mkt'],
+    products: [
+      {
+        title: 'オセロ',
+        description: 'オセロです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 61,
   },
   {
     userName: 'yossuli2',
     displayName: 'yossuli',
     graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
+    introduction: 'INIAD.tsのyossuliです。よろしくお願いします。',
+    socialLinks: ['https://github.com/yossuli', 'https://twitter.com/yossuli'],
+    products: [
+      {
+        title: 'RPG',
+        description: 'RPGです。',
+      },
+    ],
+    findy: 59,
   },
+
   {
     userName: 'g-ratie3',
     displayName: 'ラティ',
     graduateYear: 2026,
+    introduction: 'INIAD.ts代表のラティです。よろしくお願いします。',
     socialLinks: ['https://github.com/g-ratie'],
+    products: [
+      {
+        title: 'INIAD.ts入退出Bot',
+        description: 'INIAD.tsの入退出を管理するDiscord Botです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 99,
   },
   {
     userName: 'mst-mkt3',
     displayName: '🧶',
     graduateYear: 2027,
+    introduction: 'INIAD.tsの🧶です。よろしくお願いします。',
     socialLinks: ['https://github.com/mst-mkt'],
+    products: [
+      {
+        title: 'オセロ',
+        description: 'オセロです。',
+      },
+      {
+        title: 'INIAD.ts公式サイト',
+        description: 'INIAD.tsの公式サイトです。',
+      },
+    ],
+    findy: 61,
   },
   {
     userName: 'yossuli3',
     displayName: 'yossuli',
     graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
-  },
-  {
-    userName: 'g-ratie4',
-    displayName: 'ラティ',
-    graduateYear: 2026,
-    socialLinks: ['https://github.com/g-ratie'],
-  },
-  {
-    userName: 'mst-mkt4',
-    displayName: '🧶',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/mst-mkt'],
-  },
-  {
-    userName: 'yossuli4',
-    displayName: 'yossuli',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
-  },
-  {
-    userName: 'g-ratie5',
-    displayName: 'ラティ',
-    graduateYear: 2026,
-    socialLinks: ['https://github.com/g-ratie'],
-  },
-  {
-    userName: 'mst-mkt5',
-    displayName: '🧶',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/mst-mkt'],
-  },
-  {
-    userName: 'yossuli5',
-    displayName: 'yossuli',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
-  },
-  {
-    userName: 'g-ratie6',
-    displayName: 'ラティ',
-    graduateYear: 2026,
-    socialLinks: ['https://github.com/g-ratie'],
-  },
-  {
-    userName: 'mst-mkt6',
-    displayName: '🧶',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/mst-mkt'],
-  },
-  {
-    userName: 'yossuli6',
-    displayName: 'yossuli',
-    graduateYear: 2027,
-    socialLinks: ['https://github.com/yossuli'],
+    introduction: 'INIAD.tsのyossuliです。よろしくお願いします。',
+    socialLinks: ['https://github.com/yossuli', 'https://twitter.com/yossuli'],
+    products: [
+      {
+        title: 'RPG',
+        description: 'RPGです。',
+      },
+    ],
+    findy: 59,
   },
 ];
